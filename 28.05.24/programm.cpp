195984523fc07008583ee0337298d44cc63828c3
Robin <142886484+RobinTea@users.noreply.github.com>
2024-05-28 08:11:41 +0200

    Add files via upload

---
+++ 28.05.24/programm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    for (int i = 0; i <= 9; ++i) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    for (int i = 9; i >= 0; --i) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
