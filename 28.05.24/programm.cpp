d5a07ccdf37cffe067392e60f58a2af3cc2881ee
Robin <142886484+RobinTea@users.noreply.github.com>
2024-05-28 08:17:10 +0200

    Add files via upload

--- 28.05.24/programm.cpp
+++ 28.05.24/programm.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-using namespace std;
+using namespace std; // damit ich weniger schreiben muss
 
 int main()
 {
     cout << "Hello World!\n";
-    for (int i = 0; i <= 9; ++i) {
+    for (int i = 0; i <= 9; ++i) { //forloop der von 0 bis 9 zaehlt
+        cout << i << " "; //gibt Wert i und einen Abstand aus
+    }
+
+    cout << endl; //Neue Zeile
+
+    for (int i = 9; i >= 0; --i) { //forloop der von 9 bis 0 zaehlt
         cout << i << " ";
     }
 
-    cout << endl;
-
-    for (int i = 9; i >= 0; --i) {
-        cout << i << " ";
-    }
-
-    cout << endl;
-
-    return 0;
+    return 0; // Beendung des Programmes
 }
